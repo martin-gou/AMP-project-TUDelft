ced5a492f19196a142fab8298b2b582bfa9c8811
Martin Gou <martin0248159@gmail.com>
2026-03-12 01:05:54 +0100

    add sbatch sh

---
+++ src/tools/slurm_train_ours.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --job-name="cp_cam_temp_ro47020"
+#SBATCH --partition=gpu-a100-small
+#SBATCH --time=4:00:00
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=4G
+#SBATCH --gpus-per-task=1
+#SBATCH --account=education-me-courses-ro47020
+#SBATCH --mail-type=END
+#SBATCH --output=outputs/slurm_cam_temp_%j.out
+#SBATCH --error=outputs/slurm_cam_temp_%j.err
+
+set -euo pipefail
+
+mkdir -p outputs
+
+module load 2024r1 miniconda3/4.12.0 cuda/12.5
+
+unset CONDA_SHLVL
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate amp
+export WANDB_MODE=offline
+echo "Working directory: $(pwd)"
+echo "Python: $(which python)"
+echo "Started at: $(date)"
+nvidia-smi
+
+srun python -u src/tools/train.py \
+  model=centerpoint_radar_camera_temporal \
+  exp_id=centerpoint_radar_camera_temporal_slurm \
+  batch_size=1 \
+  num_workers=2 \
+  epochs=12
+
+echo "Finished at: $(date)"
+nvidia-smi
